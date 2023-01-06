e3b30936b9de28896738cffd73c8ea8e134736d5
Vijjapu Venkata Vinay <97441690+vijjapu-venkata-vinay@users.noreply.github.com>
2023-01-06 21:55:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def recommendation(movie):
         recommended_movies.append(movies.iloc[i[0]].title)
     return recommended_movies
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
-similarity = pickle.load(open('similar_y.pkl', 'rb'))
+similarity = pickle.load(open('similar.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 st.title('Movie Recommendation System')
 
